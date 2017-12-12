8d2f0599290c303b68ff36e683320d92527d3845
Mohamed Abdul Rasheed <mohideen@umd.edu>
2017-12-12 09:04:01 -0500

    LIBBCM-29. Configure hour facet as range facet.
    https://issues.umd.edu/browse/LIBBCM-29

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -79,7 +79,7 @@ class CatalogController < ApplicationController
     config.add_facet_field 'month_si', label: 'Month', limit: 5
     config.add_facet_field 'day_ii', label: 'Day', limit: 5
     config.add_facet_field 'date_dtti', label: 'Date Range', limit: 5, date: { format: :short }
-    config.add_facet_field 'hour_iti', label: 'Hour Range', limit: 5
+    config.add_facet_field 'hour_iti', label: 'Hour Range', range: { assumed_boundaries: [0,23], segments: true} 
     # End customization for LIBBCM-29
     config.add_facet_field 'genre_sim', label: 'Genres', limit: 5
     config.add_facet_field 'collection_ssim', label: 'Collection', limit: 5
