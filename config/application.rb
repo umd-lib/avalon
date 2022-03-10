bd9b91b8ae1d5531f704e74fa68c668ff99e8dac
Peter Eichman <peichman@umd.edu>
2022-03-10 11:48:10 -0500

    Bumped version to 7.2-umd-5-rc1

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'resolv-replace'
 Bundler.require(*Rails.groups)
 
 module Avalon
-  VERSION = '7.2.0-umd-2'
+  VERSION = '7.2.0-umd-5-rc1'
 
   class Application < Rails::Application
     require 'avalon/configuration'
