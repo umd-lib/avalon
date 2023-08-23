10e512340716f1079067f0558a3236700b3b3f8c
David P. Steelman <dsteelma@umd.edu>
2023-08-23 08:37:34 -0400

    LIBAVALON-313. Bumped version to 7.4.0-umd-1
    https://umd-dit.atlassian.net/browse/LIBAVALON-313

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'resolv-replace'
 Bundler.require(*Rails.groups)
 
 module Avalon
-  VERSION = '7.4.0-umd-0.1'
+  VERSION = '7.4.0-umd-1'
 
   class Application < Rails::Application
     require 'avalon/configuration'
