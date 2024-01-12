83dfe8ee73f7cf035a3be3f9ca3569c61db2c8c8
Mohamed Abdul Rasheed <mohideen@umd.edu>
2024-01-12 12:32:47 -0500

    Update Avalon version

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'resolv-replace'
 Bundler.require(*Rails.groups)
 
 module Avalon
-  VERSION = '7.4.0-umd-2.3'
+  VERSION = '7.4.0-umd-3'
 
   class Application < Rails::Application
     require 'avalon/configuration'
