1139860cfbebcc535ddd902dc1aa5398522c42b8
David P. Steelman <dsteelma@umd.edu>
2025-06-30 15:06:37 -0400

    LIBAVALON-362. Update "authentication.rb" for "domain" Setting changes
    In "config/settings.yml", the "domain" parameter was changed from
    a String ("http://av-local:3000") to a Hash
    { domain: { host: av-local, port: 3000, protocol: http } }
    
    Updated the "lib/avalon/authentication.rb" file to use the Hash
    form instead of the String form when setting up the "saml_config" hash.
    
    Did this because the String form seem to be obsolete (although still
    supported), and the String form was causing some Avalon tests to fail.
    
    Updated "environment" for "avalon" in docker-compose.yml to set the
    "domain" using individual components, instead of as a single single,
    to be compatible with the changes in the "authentication.rb".
    
    https://umd-dit.atlassian.net/browse/LIBAVALON-362

--- lib/avalon/authentication.rb
+++ lib/avalon/authentication.rb
@@ -39,7 +39,11 @@ module Avalon
     saml_config = Config.find { |conf| conf[:provider] == :saml }
     if saml_config
       saml_config[:params][:issuer] = ENV['SAML_ISSUER'] if ENV['SAML_ISSUER']
-      saml_config[:params][:assertion_consumer_service_url] = URI::join(Settings.domain, "/users/auth/saml/callback").to_s
+      domain = "#{Settings[:domain][:protocol]}://#{Settings[:domain][:host]}"
+      unless Settings[:domain][:port].blank?
+        domain = "#{domain}:#{Settings[:domain][:port]}"
+      end
+      saml_config[:params][:assertion_consumer_service_url] = URI::join(domain, "/users/auth/saml/callback").to_s
       saml_config[:params][:private_key] = ENV['SAML_SP_PRIVATE_KEY'] if ENV['SAML_SP_PRIVATE_KEY']
       saml_config[:params][:certificate] = ENV['SAML_SP_CERTIFICATE'] if ENV['SAML_SP_CERTIFICATE']
     end
