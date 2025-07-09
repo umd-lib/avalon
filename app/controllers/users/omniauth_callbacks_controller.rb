88c238aea100163a46eea8a941a2bb1cbfa1b95a
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2025-07-09 13:44:53 -0400

    LIBAVALON-379: Redirecting LTI Users to Media Player from Canvas
    Also had to disable HTTPOnly to allow the Javascript in Canvas to set
    cookies in the Avalon IFrame
    
    https://umd-dit.atlassian.net/browse/LIBAVALON-379

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,11 @@
 # Copyright 2011-2023, The Trustees of Indiana University and Northwestern
 #   University.  Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed
 #   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 #   CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -88,7 +88,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:success] = nil
       render inline: '<html><head><script>window.close();</script></head><body></body><html>'.html_safe
     else
-      redirect_to find_redirect_url(auth_type, lti_group: user_session&.dig(:lti_group))
+      # UMD Customization
+      redirect_to lti_redirect_url(auth_type, lti_group: user_session&.dig(:lti_group))
+      # End UMD Customization
     end
   end
 
