4e7c271549b45adfed066fb4a5f7d73ee92adb41
David P. Steelman <dsteelma@umd.edu>
2025-08-19 07:20:31 -0400

    LIBAVALON-396. Bumped version to 7.8.0-umd-0
    https://umd-dit.atlassian.net/browse/LIBAVALON-396

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 
 module Avalon
   # UMD Customization
-  VERSION = '7.8.0-umd-0-SNAPSHOT'
+  VERSION = '7.8.0-umd-0'
   # End UMD Customization
 
   class Application < Rails::Application
