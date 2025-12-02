2cfe6c5563f4588d1f7eb189229a98dd5041f011
Mohamed Abdul Rasheed <mohideen@umd.edu>
2025-12-01 22:05:30 -0500

    Bump version string to 7.8.0-umd-1.4
    https://umd-dit.atlassian.net/browse/LIBAVALON-478

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 
 module Avalon
   # UMD Customization
-  VERSION = '7.8.0-umd-1.3'
+  VERSION = '7.8.0-umd-1.4'
   # End UMD Customization
 
   class Application < Rails::Application
