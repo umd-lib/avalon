665f424640817675dc7e88b2d874e21c7b8c90d9
Mohamed Abdul Rasheed <mohideen@umd.edu>
2025-12-02 15:29:06 -0500

    LIBAVALON-479. Add cron job to cleanup encode files.
    Also, updated rake task to be able to parse older_than correctly for manual cleanup runs.
    
    https://umd-dit.atlassian.net/browse/LIBAVALON-479

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -24,6 +24,7 @@ Rails.application.config.to_prepare do
     # UMD Customization
     Sidekiq::Cron::Job.create(name: 'Clean up access tokens that have past their expiration date - every 1day', cron: '0 1 * * *', class: 'CleanupAccessTokenJob')
     Sidekiq::Cron::Job.create(name: 'Delete old searches - every 20 minutes', cron: '0,20,40 * * * *', class: 'DeleteOldSearchesJob')
+    Sidekiq::Cron::Job.create(name: 'Delete old encode files - every 1day', cron: '0 1 * * *', class: 'DeleteOldEncodeFilesJob')
     # End UMD Customization
 rescue Redis::CannotConnectError => e
     Rails.logger.warn "Cannot create sidekiq-cron jobs: #{e.message}"
