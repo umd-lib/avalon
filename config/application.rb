b3f0272b51558c2919abd6ec5f0ca30b9f669629
Mohamed Abdul Rasheed <mohideen@umd.edu>
2026-02-24 17:05:34 -0500

    Bumped Avalon version to '7.8.0-umd-3'

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 
 module Avalon
   # UMD Customization
-  VERSION = '7.8.0-umd-2.2'
+  VERSION = '7.8.0-umd-3'
   # End UMD Customization
 
   class Application < Rails::Application
